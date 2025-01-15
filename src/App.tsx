cfaa5b8702ab92b2b47896099758be1e2cd4ea32
KukshaBird <samoilovartem1989@gmail.com>
2025-01-16 00:25:43 +0200

    Clean css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import './App.css';
+import Header from './components/Header.tsx';
 
 function App() {
-  return <div>Hello world!</div>;
+  return (
+    <>
+      <Header />
+      <main
+        style={{
+          backgroundColor: 'lightblue',
+          margin: '0 auto',
+          padding: '2rem',
+          textAlign: 'center',
+          maxWidth: '1280px',
+          borderRadius: '10px',
+          display: 'flex',
+          placeItems: 'center',
+          minWidth: '320px',
+          minHeight: '60vh',
+          justifyContent: 'space-around',
+        }}
+      >
+        <h1>Hello World</h1>
+      </main>
+    </>
+  );
 }
 
 export default App;
