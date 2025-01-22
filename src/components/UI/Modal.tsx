52346a55ec6efa0eeb66398a1ce3bac0d46ca15d
KukshaBird <samoilovartem1989@gmail.com>
2025-01-23 00:36:25 +0200

    Added portal ato Modal.

--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -1,4 +1,5 @@
-import React, { useImperativeHandle, useRef } from 'react';
+import React, { RefObject, useImperativeHandle, useRef } from 'react';
+import { createPortal } from 'react-dom';
 
 export interface ModalDisplayHandle {
   open: () => void;
@@ -6,54 +7,40 @@ export interface ModalDisplayHandle {
 }
 
 export interface ModalProps {
-  ref: React.Ref<ModalDisplayHandle>;
+  onClose: () => void;
   children: React.ReactNode;
+  ref: RefObject<ModalDisplayHandle | null>;
 }
 
-export function Modal({ children, ref }: ModalProps): React.ReactElement {
-  const modal = useRef<HTMLDialogElement>(null);
+export function Modal({ children, onClose, ref }: ModalProps): React.ReactPortal | null {
+  const dialogRef = useRef<HTMLDialogElement>(null);
   useImperativeHandle(ref, () => {
     return {
       open() {
-        if (modal.current) {
-          modal.current.showModal();
+        if (dialogRef.current) {
+          dialogRef.current.showModal();
         }
       },
       close() {
-        if (modal.current) {
-          modal.current.close();
+        if (dialogRef.current) {
+          dialogRef.current.close();
         }
       },
     };
   }, []);
 
-  const onClose = () => {
-    if (modal.current) {
-      modal.current.close();
-    }
-  };
-
-  const handleClose = () => {
-    if (modal.current) {
-      modal.current.close();
-    }
-  };
-
-  return (
-    <dialog ref={modal} onClose={onClose}>
-      <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-50">
-        <div className="bg-white rounded-lg shadow-lg w-full max-w-md p-6">
-          {children}
-          <div className="flex justify-end space-x-4">
-            <button className="btn-secondary" onClick={handleClose}>
-              Cancel
-            </button>
-            <button className="btn-primary" onClick={handleClose}>
-              Confirm
-            </button>
-          </div>
+  const modalContainer = document.getElementById('modal-container');
+  if (!modalContainer) {
+    console.error('Modal container not found!');
+    return null;
+  } else {
+    return createPortal(
+      <dialog ref={dialogRef} onClose={onClose}>
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-50">
+          <div className="bg-white rounded-lg shadow-lg w-full max-w-md p-6">{children}</div>
         </div>
-      </div>
-    </dialog>
-  );
+      </dialog>,
+      modalContainer
+    );
+  }
 }
