5a84ef0b9e930c3217f43fcc39b503923a67f76e
KukshaBird <samoilovartem1989@gmail.com>
2025-01-23 09:40:35 +0200

    Added defaults for UserForm.

--- src/components/User/UserForm.tsx
+++ src/components/User/UserForm.tsx
@@ -7,6 +7,12 @@ interface FormProps {
   onSubmit: (data: CreateUser) => void;
 }
 
+const INIT_DATA: CreateUser = {
+  benefit: false,
+  name: '',
+  startBalance: 0,
+};
+
 export function UserForm({ onClose, onSubmit }: FormProps) {
   const handleSubmit = (event: React.SyntheticEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -34,9 +40,9 @@ export function UserForm({ onClose, onSubmit }: FormProps) {
     <form onSubmit={handleSubmit} className={'max-w-md mx-auto bg-sky-100 pb-4'}>
       <h2 className={'text-xl font-bold text-center accent-gray-700 p-4'}>Add User</h2>
       <div>
-        <Input label={'User Name'} name={'name'} />
-        <Input label={'Free Lunch'} name={'benefit'} type="checkbox" />
-        <Input label={'Balance'} name={'startBalance'} type="number" />
+        <Input label={'User Name'} name={'name'} defaultValue={INIT_DATA.name} />
+        <Input label={'Free Lunch'} name={'benefit'} type="checkbox" defaultChecked={INIT_DATA.benefit} />
+        <Input label={'Balance'} name={'startBalance'} type="number" defaultValue={INIT_DATA.startBalance} />
       </div>
       <div className="flex justify-end space-x-4 mt-4 mx-4">
         <button type={'button'} className="btn-secondary" onClick={handleClose}>
